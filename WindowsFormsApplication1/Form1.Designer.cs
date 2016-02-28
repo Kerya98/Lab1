82e678a54d47360e1717599005673542943260bc
vadymbutlerovskyi <vadym.butlerovskyi@gmail.com>
2016-02-28 21:03:53 +0000

    Update Form

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -30,12 +30,13 @@
         {
             this.richTextBoxInput = new System.Windows.Forms.RichTextBox();
             this.richTextBoxOutput = new System.Windows.Forms.RichTextBox();
+            this.textBoxFileName = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // richTextBoxInput
             // 
-            this.richTextBoxInput.Location = new System.Drawing.Point(10, 11);
-            this.richTextBoxInput.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.richTextBoxInput.Location = new System.Drawing.Point(11, 47);
+            this.richTextBoxInput.Margin = new System.Windows.Forms.Padding(2);
             this.richTextBoxInput.Name = "richTextBoxInput";
             this.richTextBoxInput.Size = new System.Drawing.Size(796, 161);
             this.richTextBoxInput.TabIndex = 0;
@@ -43,25 +44,37 @@
             // 
             // richTextBoxOutput
             // 
-            this.richTextBoxOutput.Location = new System.Drawing.Point(9, 203);
-            this.richTextBoxOutput.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.richTextBoxOutput.Location = new System.Drawing.Point(11, 235);
+            this.richTextBoxOutput.Margin = new System.Windows.Forms.Padding(2);
             this.richTextBoxOutput.Name = "richTextBoxOutput";
             this.richTextBoxOutput.Size = new System.Drawing.Size(796, 161);
             this.richTextBoxOutput.TabIndex = 1;
             this.richTextBoxOutput.Text = "";
             // 
+            // textBoxFileName
+            // 
+            this.textBoxFileName.Location = new System.Drawing.Point(12, 12);
+            this.textBoxFileName.Name = "textBoxFileName";
+            this.textBoxFileName.ReadOnly = true;
+            this.textBoxFileName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxFileName.TabIndex = 2;
+            this.textBoxFileName.Text = "index.html";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(814, 373);
+            this.BackColor = System.Drawing.Color.LightGoldenrodYellow;
+            this.ClientSize = new System.Drawing.Size(814, 407);
+            this.Controls.Add(this.textBoxFileName);
             this.Controls.Add(this.richTextBoxOutput);
             this.Controls.Add(this.richTextBoxInput);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Lab1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -69,6 +82,7 @@
 
         private System.Windows.Forms.RichTextBox richTextBoxInput;
         private System.Windows.Forms.RichTextBox richTextBoxOutput;
+        private System.Windows.Forms.TextBox textBoxFileName;
     }
 }
 
